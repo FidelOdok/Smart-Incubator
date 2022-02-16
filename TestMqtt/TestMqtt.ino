fd85be3740148328b9db83b76857bd64227f163a
FidelOdok <odokfidelis@gmail.com>
2022-02-16 16:48:52 +0100

    Addes the configuration function

---
+++ TestMqtt/TestMqtt.ino
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+#include <WiFi.h>
+
+
+const char *SSID = "Trip";
+const char *PWD = "potential";
+
+
+void setup() {
+  Serial.begin(9600);
+  connectToWiFi();
+  
+}
+
+
+void connectToWiFi() {
+  Serial.print("Connectiog to ");
+ 
+  WiFi.begin(SSID, PWD);
+  Serial.println(SSID);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.print("Connected.");
+  
+}
